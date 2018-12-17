229a080f693ee08cfbd61906150fc0d332e775ec
vemonet <vincent.emonet@gmail.com>
2018-12-17 23:25:47 +0100

    edit all insert-biolink to use var for graph URIs

--- data2services-transform-biolink.sh
+++ data2services-transform-biolink.sh
@@ -6,8 +6,6 @@ LOGIN=$1
 PASSWORD=$2
 
 # BioLink transformation
-docker run -it --rm -v "$PWD/insert-biolink":/data sparql-rdf4j-operations -rq "/data" -url "http://graphdb.dumontierlab.com/repositories/ncats-red-kg/statements" -un $LOGIN -pw $PASSWORD
+docker run -it --rm -v "$PWD/insert-biolink/drugbank":/data rdf4j-sparql-operations -rq "/data" -url "http://graphdb.dumontierlab.com/repositories/ncats-red-kg/statements" -un $LOGIN -pw $PASSWORD -var inputGraph:http://data2services/graph/xml2rdf outputGraph:http://data2services/biolink/drugbank
 
-# Insert datasets metadata 
-docker run -it --rm -v "$PWD/insert-dataset_metadata/summary":/data sparql-rdf4j-operations -rq "/data" -url "http://graphdb.dumontierlab.com/repositories/ncats-red-kg/statements" -un $LOGIN -pw $PASSWORD
-docker run -it --rm -v "$PWD/insert-dataset_metadata/distribution":/data sparql-rdf4j-operations -rq "/data" -url "http://graphdb.dumontierlab.com/repositories/ncats-red-kg/statements" -un $LOGIN -pw $PASSWORD
+docker run -it --rm -v "$PWD/insert-biolink/hgnc":/data rdf4j-sparql-operations -rq "/data" -url "http://graphdb.dumontierlab.com/repositories/ncats-red-kg/statements" -un $LOGIN -pw $PASSWORD -var inputGraph:http://data2services/graph/autor2rml outputGraph:http://data2services/biolink/hgnc
