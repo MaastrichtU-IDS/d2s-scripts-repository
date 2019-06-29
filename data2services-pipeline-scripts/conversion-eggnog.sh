93522bb9285d4d1daa548102e7dfc9e298ec2b97
vemonet <vincent.emonet@gmail.com>
2019-06-29 09:24:59 +0200

    fix split eggnog

--- data2services-pipeline-scripts/conversion-eggnog.sh
+++ data2services-pipeline-scripts/conversion-eggnog.sh
@@ -18,13 +18,15 @@ docker run -it --rm --link drill:drill \
 
 # Load RDF file in GraphDB ncats-test repository
 
-# Split Pmids:
+# Split ProteinsIds: http://graphdb.dumontierlab.com/resource?uri=https:%2F%2Fw3id.org%2Fdata2services%2Fdata%2Femonet%2Fncats%2Feggnog%2FNOG.members.tsv%2F100700
 docker run -d --name split-eggnog --link graphdb:graphdb \
   vemonet/data2services-sparql-operations -op split \
-  --split-property "https://w3id.org/data2services/autor2rml/model/Pmids" \
-  --split-delimiter ";" \
-  -ep "http://graphdb:7200/repositories/ncats-test" \
-  -uep "http://graphdb:7200/repositories/ncats-test/statements" \
+  --split-property "https://w3id.org/data2services/model/Proteinids" \
+  --split-class "https://w3id.org/data2services/data/emonet/ncats/eggnog/NOG.members.tsv" \
+  --split-delimiter "," \
+  --split-delete \
+  -ep "http://graphdb:7200" \
+  -uep "bio2vec-test" \
   -un emonet -pw PASSWORD
 
 docker run -d --name convert_eggnog --link graphdb:graphdb data2services-sparql-operations -f "https://github.com/MaastrichtU-IDS/data2services-transform-repository/tree/master/sparql/insert-biolink/eggnog" -ep "http://graphdb:7200/repositories/bio2vec/statements" -un emonet -pw $PASSWORD -var serviceUrl:http://localhost:7200/repositories/bio2vec-test inputGraph:https://w3id.org/data2services/graph/autor2rml/eggnog outputGraph:https://w3id.org/data2services/graph/biolink/eggnog
